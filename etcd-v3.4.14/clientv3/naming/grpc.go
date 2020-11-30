f3b297f3901cd92bba8ae24c099077f0e8a7196e
Vee Zhang <veezhang@126.com>
2020-11-30 18:21:48 +0800

    naming

--- etcd-v3.4.14/clientv3/naming/grpc.go
+++ etcd-v3.4.14/clientv3/naming/grpc.go
@@ -29,11 +29,20 @@ import (
 var ErrWatcherClosed = fmt.Errorf("naming: watch closed")
 
 // GRPCResolver creates a grpc.Watcher for a target to track its resolution changes.
+// GRPCResolver 实现 grpc.naming.Resolver 接口，创建 grpc.Watcher 跟踪目标服务的改变
 type GRPCResolver struct {
 	// Client is an initialized etcd client.
 	Client *etcd.Client
 }
 
+// Update 更新
+//
+// grpc.naming.Update 的结构如下
+// 	type Update struct {
+// 		Op Operation         // 操作
+// 		Addr string          // 地址
+// 		Metadata interface{} // 元数据
+// }
 func (gr *GRPCResolver) Update(ctx context.Context, target string, nm naming.Update, opts ...etcd.OpOption) (err error) {
 	switch nm.Op {
 	case naming.Add:
@@ -50,34 +59,41 @@ func (gr *GRPCResolver) Update(ctx context.Context, target string, nm naming.Upd
 	return err
 }
 
+// Resolve 创建目标服务监视器
 func (gr *GRPCResolver) Resolve(target string) (naming.Watcher, error) {
 	ctx, cancel := context.WithCancel(context.Background())
 	w := &gRPCWatcher{c: gr.Client, target: target + "/", ctx: ctx, cancel: cancel}
 	return w, nil
 }
 
+// gRPCWatcher 监控特定服务的更新
 type gRPCWatcher struct {
-	c      *etcd.Client
-	target string
-	ctx    context.Context
-	cancel context.CancelFunc
-	wch    etcd.WatchChan
-	err    error
+	c      *etcd.Client       // 客户端
+	target string             // 目标服务
+	ctx    context.Context    // ctx
+	cancel context.CancelFunc // 取消函数
+	wch    etcd.WatchChan     // 等待事件通知 chan ( <-chan WatchResponse )
+	err    error              // 错误
 }
 
 // Next gets the next set of updates from the etcd resolver.
 // Calls to Next should be serialized; concurrent calls are not safe since
 // there is no way to reconcile the update ordering.
+// Next 从 etcd resolver 获取下一组更新。对 Next 的调用应序列化；并发调用并不安全，因为无法协调更新顺序。
+// Next 会阻塞，直到发生更新或错误。 它可能会返回一个或多个更新。 第一次调用应获得完整的结果集。 当且仅当 Watcher 无法恢复时，它应该返回错误。
 func (gw *gRPCWatcher) Next() ([]*naming.Update, error) {
 	if gw.wch == nil {
 		// first Next() returns all addresses
+		// 第一次，返回所有的地址，使用了延迟操作
 		return gw.firstNext()
 	}
+	// 出错
 	if gw.err != nil {
 		return nil, gw.err
 	}
 
 	// process new events on target/*
+	// 处理 target/* 下的事件
 	wr, ok := <-gw.wch
 	if !ok {
 		gw.err = status.Error(codes.Unavailable, ErrWatcherClosed.Error())
@@ -87,6 +103,7 @@ func (gw *gRPCWatcher) Next() ([]*naming.Update, error) {
 		return nil, gw.err
 	}
 
+	// 处理多个更新事件，并返回
 	updates := make([]*naming.Update, 0, len(wr.Events))
 	for _, e := range wr.Events {
 		var jupdate naming.Update
@@ -108,14 +125,18 @@ func (gw *gRPCWatcher) Next() ([]*naming.Update, error) {
 	return updates, nil
 }
 
+// firstNext 第一次调用 Next 时候的处理
 func (gw *gRPCWatcher) firstNext() ([]*naming.Update, error) {
 	// Use serialized request so resolution still works if the target etcd
 	// server is partitioned away from the quorum.
+	// 使用序列化的请求
+	// 获取前缀为 target 的值
 	resp, err := gw.c.Get(gw.ctx, gw.target, etcd.WithPrefix(), etcd.WithSerializable())
 	if gw.err = err; err != nil {
 		return nil, err
 	}
 
+	// 统计结果
 	updates := make([]*naming.Update, 0, len(resp.Kvs))
 	for _, kv := range resp.Kvs {
 		var jupdate naming.Update
@@ -125,9 +146,11 @@ func (gw *gRPCWatcher) firstNext() ([]*naming.Update, error) {
 		updates = append(updates, &jupdate)
 	}
 
+	// 然后创建监控，只有第一次调用后才会设置 wch
 	opts := []etcd.OpOption{etcd.WithRev(resp.Header.Revision + 1), etcd.WithPrefix(), etcd.WithPrevKV()}
 	gw.wch = gw.c.Watch(gw.ctx, gw.target, opts...)
 	return updates, nil
 }
 
+// Close 关闭监控
 func (gw *gRPCWatcher) Close() { gw.cancel() }
